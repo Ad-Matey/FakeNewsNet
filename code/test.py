1f2dc6abff820efdfa25182972eaed23a79c3973
Aditya Matey <Tcs126@TxState.edu>
2025-04-14 17:10:12 -0500

    Updated test.py

--- code/test.py
+++ code/test.py
@@ -3,21 +3,39 @@ from transformers import AutoTokenizer, BertForSequenceClassification
 
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
-modelTouse = input('Model to use (politifact - p, gossipcop - g, both - b): ')
+modelToUse = input('Model to use (politifact - p, gossipcop - g, both - b): ')
 
-if (modelTouse == 'p'):
-    model = BertForSequenceClassification.from_pretrained('politifactModel/')
+# Used to convert from model prediction to actual label of data
+dataConversion = {
+    0 : "Real",
+    1 : "Fake"
+}
+
+if (modelToUse == 'p'):
+    model = BertForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path='results/politifact/politifact_model/', local_files_only=True)
 elif (modelToUse == 'g'):
-    model = BertForSequenceClassification.from_pretrained('gossipcopModel/')
+    model = BertForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path='results/gossipcop/gossipcop_model/', local_files_only=True)
 elif (modelToUse == 'b'):
-    model = BertForSequenceClassification.from_pretrained('bothModel/')
+    model = BertForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path='results/combined/combined_model/', local_files_only=True)
+else:
+    raise ValueError("Invalid model choice")
 
-input = input('Input to Model: ')
-input = tokenizer(input, return_tensors="pt")
+repeat = True
+while repeat:
 
-model.eval()
-with torch.no_grad():
-    logits = model(**input).logits
+    textInput = input('\nInput to Model: ')
+    textInput = tokenizer(textInput, padding=True, truncation=True, return_tensors="pt")
 
-predicted_class_id = logits.argmax().item()
-print(model.config.id2label[predicted_class_id])
+    model.eval()
+    with torch.no_grad():
+        logits = model(**textInput).logits
+
+    predicted_class_id = logits.argmax().item()
+    print(dataConversion[predicted_class_id])
+
+    repeatInput = input('Enter another value (y/n)? ')
+    if (repeatInput.lower().strip() != 'y'):
+        repeat = False
